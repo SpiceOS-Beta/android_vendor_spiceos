542353db6675f57586cd0728ae6aa9797e00f09f
Han Wang <416810799@qq.com>
2020-12-05 08:30:01 +0100

    prebuilt: Remove content-types.properties
     * This is now moved to fw/b and should be overriden in
       vendor.mime.types.
    
    Change-Id: I0546763be34567816b6e87f2a721f1445c83eaf8

--- config/common.mk
+++ config/common.mk
@@ -51,10 +51,6 @@ PRODUCT_COPY_FILES += \
 $(foreach f,$(wildcard vendor/lineage/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))
 
-# Copy over added mimetype supported in libcore.net.MimeUtils
-PRODUCT_COPY_FILES += \
-    vendor/lineage/prebuilt/common/lib/content-types.properties:$(TARGET_COPY_OUT_SYSTEM)/lib/content-types.properties
-
 # Gestures
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
