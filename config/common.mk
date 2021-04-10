4797527710cffddf91f0a9e9fd7cb9b8d8ca25e6
Pranav Vashi <neobuddy89@gmail.com>
2021-04-11 00:42:37 +0530

    SpiceOS:Bring FaceUnlock Support
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,7 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/overlays/config.mk)
+$(call inherit-product-if-exists, external/faceunlock/config.mk)
 
 PRODUCT_BRAND ?= SpiceOS
 
