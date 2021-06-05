9580e7adbb5f9f4744c1e1f5cb282df514894b58
Han Wang <416810799@qq.com>
2021-06-05 11:12:50 +0200

    config: Actually disable strict mode for non eng builds
    Change-Id: If64c42111c75908f2c590199b5cfb45266af93c4

--- config/common.mk
+++ config/common.mk
@@ -19,13 +19,12 @@ endif
 #ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
-
+# else
+# Enable ADB authentication
+# PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 # Disable extra StrictMode features on all non-engineering builds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
-else
-# Enable ADB authentication
-#PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
-#endif
+# endif
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
