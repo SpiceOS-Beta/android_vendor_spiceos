3a9076b11914f5f70859cd23680e14f1bb44c600
Anush02198 <Anush.4376@gmail.com>
2021-06-21 01:24:31 +0530

    SpiceOS:Switch to Launcher3
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Change-Id: I7bf4a70398df49c4a5131a532c2352eb6c9b1149

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -27,19 +27,9 @@ PRODUCT_PACKAGES += \
     AudioFX
 endif
 
-ifeq ($(PRODUCT_TYPE), go)
+#Launcher
 PRODUCT_PACKAGES += \
-    TrebuchetQuickStepGo
-
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    TrebuchetQuickStepGo
-else
-PRODUCT_PACKAGES += \
-    TrebuchetQuickStep
-
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    TrebuchetQuickStep
-endif
+		Launcher3QuickStep
 
 # Accents
 PRODUCT_PACKAGES += \
